fff8be4cb7929629feb75ac9b98b5bd2f05a6fc5
Garrom <garrom@GarromMachine>
2018-10-16 21:15:00 +0200

    Pushed limit of X,Y,H and W to 12000. Added anti-idiot checks.

--- addons/bit_map_font_creator/main.gd
+++ addons/bit_map_font_creator/main.gd
@@ -3,7 +3,8 @@ extends EditorPlugin
 var dock
 var sav_path_line
 var tex_path_line
-var can = false
+var valid_file = false
+var valid_texture = false
 enum mode {new,edit,none}
 var actual_mode = mode.none
 func _enter_tree():
@@ -11,31 +12,84 @@ func _enter_tree():
 	add_control_to_dock(DOCK_SLOT_LEFT_UL, dock)
 	sav_path_line = dock.get_node("sav_path")
 	sav_path_line.connect("text_changed",self,"look_for_file")
+	sav_path_line.connect("text_entered",self,"look_for_file")
+	dock.get_node("tex_path").connect("text_changed",self,"look_for_texture")
+	dock.get_node("tex_path").connect("text_entered",self,"look_for_texture")
 	dock.get_node("Button").connect("button_down",self,"create")
+	look_for_texture(dock.get_node("tex_path").text)
+	look_for_file(dock.get_node("sav_path").text)
+func _process(delta):
+	if valid_file == true && valid_texture == true:
+		dock.get_node("Button").disabled = false
+	else:
+		dock.get_node("Button").disabled = true
+	
+func look_for_texture(var text):
+	if actual_mode != mode.edit:
+		if text != "" && ("res://" in text || "usr://" in text) && (".png" in text || ".jpg" in text || ".jpeg" in text):
+			if File.new().file_exists(text):
+				var l = load(text)
+				if l.get_class() != "StreamTexture":
+					dock.get_node("Label2").add_color_override("font_color", Color("ff4646"))
+					dock.get_node("Label2").text = "Texture path-Invalid"
+					valid_texture = false
+				else:
+					dock.get_node("Label2").add_color_override("font_color", Color("46ff49"))
+					dock.get_node("Label2").text = "Texture path-Valid"
+					valid_texture = true
+			else:
+				dock.get_node("Label2").add_color_override("font_color", Color("ff4646"))
+				dock.get_node("Label2").text = "Texture path-Invalid"
+				valid_texture = false
+		else:
+			dock.get_node("Label2").add_color_override("font_color", Color("ff4646"))
+			dock.get_node("Label2").text = "Texture path-Invalid"
+			valid_texture = false
+	elif actual_mode == mode.edit:
+		dock.get_node("Label2").add_color_override("font_color", Color("ffb246"))
+		dock.get_node("Label2").text = "Texture path-Not required"
+		valid_texture = true
 
 func look_for_file(var text):
-	if File.new().file_exists(text):
-		var l = load(text)
-		if l.get_class() == "BitmapFont":
-			dock.get_node("Label4").text = "File found, will edit it"
-			dock.get_node("Button").disabled = false
-			dock.get_node("Button").text = "Add char"
-			dock.get_node("Label4").add_color_override("font_color", Color("ffffff"))
-			dock.get_node("tex_path").text = "Not needed"
-			dock.get_node("tex_path").editable = false
-			actual_mode = mode.edit
+	if text != "" && ("res://" in text || "usr://" in text) && ".tres" in text:
+		if File.new().file_exists(text):
+			var l = load(text)
+			if l.get_class() == "BitmapFont":
+				dock.get_node("Label4").text = "File found, will edit it"
+				dock.get_node("Button").text = "Add char"
+				dock.get_node("Label4").add_color_override("font_color", Color("ffffff"))
+				dock.get_node("tex_path").text = "Not needed"
+				dock.get_node("tex_path").editable = false
+				dock.get_node("Label").add_color_override("font_color", Color("46ff49"))
+				dock.get_node("Label").text = "Save path-Valid"
+				actual_mode = mode.edit
+				look_for_texture(dock.get_node("tex_path").text)
+				valid_file = true
+			else:
+				dock.get_node("Label4").text = "File found, not bitmap font, Can't edit"
+				dock.get_node("Label4").add_color_override("font_color", Color("ff0000"))
+				dock.get_node("tex_path").editable = true
+				dock.get_node("tex_path").text = ""
+				dock.get_node("Label").add_color_override("font_color", Color("ff4646"))
+				dock.get_node("Label").text = "Save path-Invalid"
+				valid_file = false
 		else:
-			dock.get_node("Label4").text = "File found, not bitmap font, Can't edit"
-			dock.get_node("Label4").add_color_override("font_color", Color("ff0000"))
-			dock.get_node("Button").disabled = true
+			dock.get_node("Label4").text = "File not found, will create one"
+			dock.get_node("Button").text = "Create file"
 			dock.get_node("tex_path").editable = true
+			dock.get_node("Label4").add_color_override("font_color", Color("ffffff"))
+			dock.get_node("tex_path").text = ""
+			dock.get_node("Label").add_color_override("font_color", Color("46ff49"))
+			dock.get_node("Label").text = "Save path-Valid"
+			actual_mode = mode.new
+			valid_file = true
 	else:
-		dock.get_node("Label4").text = "File not found, will create one"
-		dock.get_node("Button").disabled = false
-		dock.get_node("Button").text = "Create file"
-		dock.get_node("tex_path").editable = true
-		dock.get_node("Label4").add_color_override("font_color", Color("ffffff"))
-		actual_mode = mode.new
+		dock.get_node("Label4").text = "Path/format not valid"
+		dock.get_node("Label4").add_color_override("font_color", Color("ff0000"))
+		dock.get_node("Label").add_color_override("font_color", Color("ff4646"))
+		dock.get_node("Label").text = "Save path-Invalid"
+		valid_file = true
+		actual_mode = mode.none
 
 func create():
 	if actual_mode != mode.none:
